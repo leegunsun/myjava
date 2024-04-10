ac7d998507e0d2f6fe5b4a5490ae7a636d25c009
leegunsun <talkcjstk@naver.com>
2024-04-10 16:37:12 +0900

    fix

--- src/main/java/com/example/demo/UserRepository.java
+++ src/main/java/com/example/demo/UserRepository.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.stereotype.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+//
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 }
