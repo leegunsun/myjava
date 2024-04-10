76fb13b27aa5f123689f16c3e85a4ca2e5b9f575
leegunsun <talkcjstk@naver.com>
2024-04-10 16:23:28 +0900

    save point

--- src/main/java/com/example/testspringboot/demo/UserRepository.java
+++ src/main/java/com/example/testspringboot/demo/UserRepository.java
@@ -1,2 +1,8 @@
-package com.example.testspringboot.demo;public class UserRepository {
+package com.example.testspringboot.demo;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
 }
--- src/main/java/com/example/testspringboot/demo/UserService.java
+++ src/main/java/com/example/testspringboot/demo/UserService.java
@@ -1,2 +1,17 @@
-package com.example.testspringboot.demo;public class UserService {
+package com.example.testspringboot.demo;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
 }
