77286a604eb0097af792442189143f43bfd94ac3
leegunsun <talkcjstk@naver.com>
2024-04-10 14:38:18 +0900

    t

--- src/main/java/com/example/testspringboot/demo/Car.java
+++ src/main/java/com/example/testspringboot/demo/Car.java
@@ -6,7 +6,7 @@ public class Car {
     public void setEngine(Engine engine) {
         this.engine = engine;
     }
-
+//
     public void drive() {
         engine.start();
         System.out.println("Car is moving!!!");
